fc4c6068c9193316df6d1a58b6f8c7529d4ef037
lbp0200 <lbp@lbp.im>
2014-03-28 21:59:18 +0800

    1

---
+++ MultiThreadingTest/TestData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiThreadingTest
+{
+    public class TestData
+    {
+        static TestData DataDic { get; set; }
+
+        static Dictionary<int, int> DataList;
+        TestData()
+        {
+            DataList = new Dictionary<int, int>();
+            for (int i = 0; i < 100; i++)
+            {
+                DataList.Add(i, i);
+            }
+        }
+
+        static int GetData()
+        {
+            Random rd = new Random();
+            int index = rd.Next(DataList.Count);
+            if (DataList.Keys.Count == 1)
+            {
+
+            }
+            return 0;
+        }
+    }
+}
